4e10302bec32f3cc10f409b35db8e520aa2abae5
Harshada Jenekar <harshadajenekar16@gmail.com>
2023-08-09 06:48:31 -0700

    code added

---
+++ src/com/harshada1/Demo1.java
@@ -0,0 +1,13 @@
+package com.harshada1;
+
+import com.harshada2.Demo1;
+
+public class Demo1 extends Demo {
+
+	static void Car() {
+		System.out.println("This is a maroti car");
+		
+	}
+	
+	
+}
---
+++ src/com/harshada1/Demo2.java
@@ -0,0 +1,16 @@
+package com.harshada1;
+
+public class Demo2 extends Demo1 {
+	@Override
+    static void Car() {
+		System.out.println("This is a alto car");
+    }
+		
+		public static void main(String[] args) {
+			Demo2.Car();
+			Demo1.Car();
+			
+		}
+		
+	}
+
